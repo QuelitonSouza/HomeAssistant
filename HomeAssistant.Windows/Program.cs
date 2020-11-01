4755f1244ccbcb229ba01c37fe1caa78c620b75a
Queliton <queliton.pires@fulltechpro.com.br>
2020-11-01 11:34:35 -0300

    Change run project. setup install

--- HomeAssistant.Windows/Program.cs
+++ HomeAssistant.Windows/Program.cs
@@ -1,112 +1,32 @@
-﻿using HomeAssistant.Windows.Config;
-using HomeAssistant.Windows.Enums;
-using HomeAssistant.Windows.Forms;
-using HomeAssistant.Windows.Services;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace HomeAssistant.Windows
 {
 	public class Program
 	{
-
-		public static Thread _thrNotifyMenu;
-
-		public static void RunMenuHomeAssistante()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new SettingsApplicationContext());
-		}
-
 		[STAThread]
 		public static void Main(string[] args)
 		{
-
-
-
-#if (DEBUG)
-
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new SettingsApplicationContext());
-
-			Service service = new Service();
-			service.Start(args);
-			Thread.Sleep(Timeout.Infinite);
-			service.Stop();
-#else
-
 			try
 			{
-				//Thread.Sleep(TimeSpan.FromSeconds(30));
+#if !DEBUG
+				Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+				key.DeleteValue("HOMEASSISTANT", false);
 
-				//if (_thrNotifyMenu == null || !_thrNotifyMenu.IsAlive)
-				//{
-				//	_thrNotifyMenu = new Thread(RunMenuHomeAssistante);
-				//	_thrNotifyMenu.IsBackground = true;
-				//	_thrNotifyMenu.Name = "Thread menu HomeAssistant";
-				//	_thrNotifyMenu.Start();
-				//}
+				//Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+				key.SetValue("HOMEASSISTANT", Application.ExecutablePath);
+#endif
 
-				ServiceBase[] ServicesToRun;
-				ServicesToRun = new ServiceBase[]
-				{
-				new Service()
-				};
-				ServiceBase.Run(ServicesToRun);
+
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+				Application.Run(new SettingsApplicationContext());
 			}
-			catch(Exception ex)
+			catch (Exception ex)
 			{
 				ex.Message.ToString();
 			}
-#endif
-		}
-	}
-
-	public class SettingsApplicationContext : ApplicationContext
-	{
-		private NotifyIcon trayIcon;
-
-		public SettingsApplicationContext()
-		{
-			trayIcon = new NotifyIcon()
-			{
-				Icon = new Icon(this.GetType(), "home.ico"),
-				ContextMenu = new ContextMenu(new MenuItem[] {
-				new MenuItem("Settings", Openmenu),
-				new MenuItem("Exit", Exit),
-
-			}),
-				Visible = true
-			};
-#if (DEBUG)
-			trayIcon.Text = "Home Assistant - TESTE";
-			trayIcon.Visible = true;
-#else
-			trayIcon.Text = "Home Assistant";
-			trayIcon.Visible = true;
-#endif
-		}
-
-
-		void Openmenu(object sender, EventArgs e)
-		{
-			var form = new FormMenu();
-			form.ShowDialog();
-		}
-
-		void Exit(object sender, EventArgs e)
-		{
-			trayIcon.Visible = false;
-			Application.Exit();
 		}
 	}
 }
---
+++ HomeAssistant.Windows/SettingsApplicationContext.cs
@@ -0,0 +1,59 @@
+﻿using HomeAssistant.Windows.Config;
+using HomeAssistant.Windows.Enums;
+using HomeAssistant.Windows.Forms;
+using HomeAssistant.Windows.Services;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeAssistant.Windows
+{
+	public class SettingsApplicationContext : ApplicationContext
+	{
+		private NotifyIcon trayIcon;
+		private ConfigService service;
+
+		public SettingsApplicationContext()
+		{
+			trayIcon = new NotifyIcon()
+			{
+				Icon = new Icon(this.GetType(), "home.ico"),
+				ContextMenu = new ContextMenu(new MenuItem[] {
+					new MenuItem("Configurações", Openmenu),
+					new MenuItem("Fechar", Exit),
+
+				})
+			};
+
+#if (DEBUG)
+			trayIcon.Text = "Home Assistant - TESTE";
+			trayIcon.Visible = true;
+#else
+			trayIcon.Text = "Home Assistant";
+			trayIcon.Visible = true;
+#endif
+
+			service = new ConfigService();
+			service.Star();
+		}
+
+
+		void Openmenu(object sender, EventArgs e)
+		{
+			var form = new FormMenu();
+			form.ShowDialog();
+		}
+
+		void Exit(object sender, EventArgs e)
+		{
+			trayIcon.Visible = false;
+			Application.Exit();
+		}
+	}
+}
